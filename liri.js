7c9caeb406a75c1ab99678fe747c783205c0fb45
Kyle Knox <kyleknox86@gmail.com>
2019-05-14 18:34:41 -0600

    spotify function

--- liri.js
+++ liri.js
@@ -5,8 +5,7 @@ require("dotenv").config();
 var keys = require("./key.js");
 var axios = require("axios");
 
-//ACCESS SPOTIFY KEY
-// var spotify = new Spotify(keys.spotify);
+
 
 //SWITCH STATEMENT TO DEFINE ARGUMENTS
 var action = process.argv[2];
@@ -39,14 +38,13 @@ function concertThis() {
 
         .then(function (response) {
             var booty = response.data[0];
-            console.log(booty);
             console.log("----------------------------------------------------------------");
-            console.log(booty.venue.name);
+            console.log("Venue Name: " + booty.venue.name);
             console.log("----------------------------------------------------------------");
-            console.log(booty.venue.country);
+            console.log("Country: " + booty.venue.country);
             console.log("----------------------------------------------------------------");
-
-            console.log(moment(booty.datetime).format("MM/DD/YYYY"));
+            console.log("Event Time: " + moment(booty.datetime).format("MM/DD/YYYY"));
+            console.log("----------------------------------------------------------------");
 
         })
         .catch(function (error) {
@@ -56,6 +54,37 @@ function concertThis() {
 
 function spotify() {
 
+    if (input === undefined) {
+        input = "The Sign Ace of Base"
+    }
+    //ACCESS SPOTIFY KEY
+    var Spotify = require('node-spotify-api');
+    var spotify = new Spotify(keys.spotify);
+
+    spotify
+        .search({
+            type: 'track',
+            query: input
+        })
+        .then(function (response) {
+            var spoils = response.tracks.items[0];
+            console.log(spoils);
+            console.log("----------------------------------------------------------------");
+            console.log(spoils.album.artists[0].name);
+            console.log("----------------------------------------------------------------");
+            console.log(spoils.name);
+            console.log("----------------------------------------------------------------");
+            console.log(spoils.preview_url);
+            console.log("----------------------------------------------------------------");
+            console.log(spoils.album.name);
+            console.log("----------------------------------------------------------------");
+
+
+
+        })
+        .catch(function (err) {
+            console.log(err);
+        });
 }
 
 function movie() {
